60258efc24961934c38c8e4b17fcd56d4ef894f8
kier turpin <kier.turpin@student.uagc.edu>
2026-03-24 20:57:06 -0400

    Initial Commit

---
+++ MoodMix-iOS/MoodMix_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMix_iOSApp.swift
+//  MoodMix-iOS
+//
+//  Created by Penny on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct MoodMix_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
